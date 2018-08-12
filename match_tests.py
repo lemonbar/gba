21616675f2e99ef3af9852aa3d7882906724ef06
lemonbar <lemon_bar@163.com>
2018-08-12 13:02:36 +0800

    add unit test

--- match_tests.py
+++ match_tests.py
@@ -22,5 +22,16 @@ class MatchWinInarowTestCase(TeamMatchTestCase):
         self.assertEqual(tm.best_inarow[1],2,'team 1 win 2 in a row')
         self.assertEqual(tm.best_inarow[0],0,'team 0 win 0 in a row')
 
+class MatchWinnersTestCase(TeamMatchTestCase):
+    def test_winners_time(self):
+        tm = MatchResult(self.teams)
+        tm.add_match(0,7,1,4)
+        tm.add_match(0,7,2,3)
+        self.assertEqual(tm.winners[0],2,'team 0 winners 2 matches')
+        self.assertEqual(tm.winners[1],0,'team 1 winners 0 matches')
+        self.assertEqual(tm.losers[1],1,'team 1 losers 1 matches')
+        self.assertEqual(tm.winners[2],0,'team 2 winners 0 matches')
+        self.assertEqual(tm.losers[2],1,'team 2 losers 1 matches')
+
 if __name__ == '__main__':
     unittest.main()
